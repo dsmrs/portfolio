140a817690ede3c75be3093a76964484375b6c56
Dorian Somers <doriansomers@gmail.com>
2018-03-13 18:17:07 +0100

    Automated commit at 2018-03-13 17:17:06 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/index-7cc96066.js
+++
@@ -1,9 +0,0 @@
-// import Reveal from 'reveal.js';
-import $ from 'jquery';
-import 'bootstrap';
-import { init } from './slider';
-// Reveal.initialize({ transition: 'zoom' });
-const toggle = $('[data-toggle="tooltip"]');
-console.log(toggle);
-toggle.tooltip();
-init();
